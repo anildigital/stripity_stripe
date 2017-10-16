096c188f050e033c5a07004a16fd9b3fcb0c6a39
Matt Jadczak <matt@mjdk.co.uk>
2017-10-16 02:27:07 +0100

    Add LineItem object

--- lib/stripe/subscriptions/line_item.ex
+++ lib/stripe/subscriptions/line_item.ex
@@ -5,12 +5,44 @@ defmodule Stripe.LineItem do
   Stripe API reference: https://stripe.com/docs/api/ruby#invoice_line_item_object
   """
 
-  @type t :: %__MODULE__{}
+  @type t :: %__MODULE__{
+               id: Stripe.id,
+               object: String.t,
+               amount: integer,
+               currency: String.t,
+               description: String.t,
+               discountable: boolean,
+               livemode: boolean,
+               metadata: %{
+                 optional(String.t) => String.t
+               },
+               period: %{
+                 start: Stripe.timestamp,
+                 end: Stripe.timestamp
+               },
+               plan: Stripe.Plan.t | nil,
+               proration: boolean,
+               quantity: integer,
+               subscription: Stripe.id | nil,
+               subscription_item: Stripe.id | nil,
+               type: :invoiceitem | :subscription
+             }
 
   defstruct [
-    :id, :object,
-    :amount, :currency, :description, :discountable, :livemode, :metadata,
-    :period, :plan, :proration, :quantity, :subscription, :subscription_item,
+    :id,
+    :object,
+    :amount,
+    :currency,
+    :description,
+    :discountable,
+    :livemode,
+    :metadata,
+    :period,
+    :plan,
+    :proration,
+    :quantity,
+    :subscription,
+    :subscription_item,
     :type
   ]
 end
