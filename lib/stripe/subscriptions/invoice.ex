1aed2f85e75e2750b790274106affcb5f6e0ed06
Matt Jadczak <matt@mjdk.co.uk>
2017-10-16 02:22:52 +0100

    Add Invoice object

--- lib/stripe/subscriptions/invoice.ex
+++ lib/stripe/subscriptions/invoice.ex
@@ -12,20 +12,79 @@ defmodule Stripe.Invoice do
 
   Stripe API reference: https://stripe.com/docs/api#invoice
   """
-
+  use Stripe.Entity
   alias Stripe.Util
 
-  @type t :: %__MODULE__{}
+  @type t :: %__MODULE__{
+               id: Stripe.id,
+               object: String.t,
+               amount_due: integer,
+               application_fee: integer,
+               attempt_count: non_neg_integer,
+               attempted: boolean,
+               charge: Stripe.id | Stripe.Charge.t,
+               closed: boolean,
+               currency: String.t,
+               customer: Stripe.id,
+               date: Stripe.timestamp,
+               description: String.t,
+               discount: Stripe.Discount.t,
+               ending_balance: integer,
+               forgiven: boolean,
+               lines: Stripe.List.of(Stripe.LineItem.t),
+               livemode: boolean,
+               metadata: %{
+                 optional(String.t) => String.t
+               },
+               next_payment_attempt: Stripe.timestamp,
+               paid: boolean,
+               period_end: Stripe.timestamp,
+               period_start: Stripe.timestamp,
+               receipt_number: String.t,
+               starting_balance: integer,
+               statement_descriptor: String.t,
+               subscription: Stripe.id | Stripe.Subscription.t,
+               subscription_proration_date: Stripe.timestamp,
+               subtotal: integer,
+               tax: integer | nil,
+               tax_percent: integer | nil,
+               total: integer,
+               webhooks_delivered_at: Stripe.timestamp,
+             }
 
   defstruct [
-    :id, :object,
-    :amount_due, :application_fee, :attempt_count, :attempted,
-    :charge, :closed, :currency, :customer, :date, :description, :discount,
-    :ending_balance, :forgiven, :lines, :livemode, :metadata,
-    :next_payment_attempt, :paid, :period_end, :period_start,
-    :receipt_number, :starting_balance, :statement_descriptor,
-    :subscription, :subscription_proration_date, :subtotal, :tax,
-    :tax_percent, :total, :webhooks_delivered_at
+    :id,
+    :object,
+    :amount_due,
+    :application_fee,
+    :attempt_count,
+    :attempted,
+    :charge,
+    :closed,
+    :currency,
+    :customer,
+    :date,
+    :description,
+    :discount,
+    :ending_balance,
+    :forgiven,
+    :lines,
+    :livemode,
+    :metadata,
+    :next_payment_attempt,
+    :paid,
+    :period_end,
+    :period_start,
+    :receipt_number,
+    :starting_balance,
+    :statement_descriptor,
+    :subscription,
+    :subscription_proration_date,
+    :subtotal,
+    :tax,
+    :tax_percent,
+    :total,
+    :webhooks_delivered_at
   ]
 
   @plural_endpoint "invoices"
